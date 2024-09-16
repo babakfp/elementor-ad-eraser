d4a88e1c318a4a5fa4d3ca3e797f470af2729c1c
babakfp <babak.bxf@gmail.com>
2024-09-16 21:54:22 +0330

    .

--- includes/core.php
+++ includes/core.php
@@ -12,7 +12,7 @@ if (!class_exists("Elementor_Ad_Eraser")) {
             add_action("admin_enqueue_scripts", function () {
                 if (is_plugin_active("elementor/elementor.php")) {
                     wp_enqueue_style(
-                        "elementor-ad-eraser-admin-ui",
+                        "elementor-ad-eraser--admin-ui",
                         Elementor_Ad_Eraser_Globals::url() .
                             "static/css/admin-ui.css",
                         [],
@@ -20,10 +20,28 @@ if (!class_exists("Elementor_Ad_Eraser")) {
                     );
                 }
             });
-            // add_action( "elementor/editor/wp_head", [ $this, "farsi_font_face" ] );
-            // add_action( "elementor/editor/after_enqueue_styles", fn() => wp_enqueue_style ( "elementor-ad-eraser::elementor-editor", Elementor_Ad_Eraser_Globals::url() . "static/css/elementor-editor.css", [], Elementor_Ad_Eraser_Globals::$version ) );
-            // add_action( "elementor/preview/enqueue_styles", fn() => wp_enqueue_style ( "elementor-ad-eraser::elementor-preview", Elementor_Ad_Eraser_Globals::url() . "static/css/elementor-preview.css", [], Elementor_Ad_Eraser_Globals::$version ) );
-            // add_action( "elementor/editor/after_enqueue_scripts", fn() => wp_enqueue_script ( "elementor-ad-eraser::elementor-editor", Elementor_Ad_Eraser_Globals::url() . "static/js/elementor-editor.js", [], Elementor_Ad_Eraser_Globals::$version ) );
+
+            add_action(
+                "elementor/editor/after_enqueue_styles",
+                fn() => wp_enqueue_style(
+                    "elementor-ad-eraser--elementor-editor",
+                    Elementor_Ad_Eraser_Globals::url() .
+                        "static/css/elementor-editor.css",
+                    [],
+                    Elementor_Ad_Eraser_Globals::$version
+                )
+            );
+
+            add_action(
+                "elementor/preview/enqueue_styles",
+                fn() => wp_enqueue_style(
+                    "elementor-ad-eraser--elementor-preview",
+                    Elementor_Ad_Eraser_Globals::url() .
+                        "static/css/elementor-preview.css",
+                    [],
+                    Elementor_Ad_Eraser_Globals::$version
+                )
+            );
         }
     }
 

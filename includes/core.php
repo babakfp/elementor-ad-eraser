bac4f6bafdb7b30d044497f08e90891ca7238859
babakfp <babak.bxf@gmail.com>
2024-09-17 11:58:25 +0330

    Dequeue elementor ai styles and scripts

--- includes/core.php
+++ includes/core.php
@@ -12,48 +12,101 @@ if (!class_exists("Elementor_Ad_Eraser")) {
     {
         public function __construct()
         {
-            add_action("admin_enqueue_scripts", function () {
-                if (is_gutenberg_active()) {
-                    wp_enqueue_style(
-                        "elementor-ad-eraser--gutenberg",
-                        Elementor_Ad_Eraser_Globals::url() .
-                            "static/css/gutenberg.css",
-                        [],
-                        Elementor_Ad_Eraser_Globals::$version
-                    );
-                }
+            // add_action("admin_enqueue_scripts", function () {
+            //     if (is_gutenberg_active()) {
+            //         wp_enqueue_style(
+            //             "elementor-ad-eraser--gutenberg",
+            //             Elementor_Ad_Eraser_Globals::url() .
+            //                 "static/css/gutenberg.css",
+            //             [],
+            //             Elementor_Ad_Eraser_Globals::$version
+            //         );
+            //     }
 
-                if (is_plugin_active("elementor/elementor.php")) {
-                    wp_enqueue_style(
-                        "elementor-ad-eraser--admin-ui",
-                        Elementor_Ad_Eraser_Globals::url() .
-                            "static/css/admin-ui.css",
-                        [],
-                        Elementor_Ad_Eraser_Globals::$version
-                    );
-                }
-            });
+            //     if (is_plugin_active("elementor/elementor.php")) {
+            //         wp_enqueue_style(
+            //             "elementor-ad-eraser--admin-ui",
+            //             Elementor_Ad_Eraser_Globals::url() .
+            //                 "static/css/admin-ui.css",
+            //             [],
+            //             Elementor_Ad_Eraser_Globals::$version
+            //         );
+            //     }
+            // });
+
+            // add_action(
+            //     "elementor/editor/after_enqueue_styles",
+            //     fn() => wp_enqueue_style(
+            //         "elementor-ad-eraser--elementor-editor",
+            //         Elementor_Ad_Eraser_Globals::url() .
+            //             "static/css/elementor-editor.css",
+            //         [],
+            //         Elementor_Ad_Eraser_Globals::$version
+            //     )
+            // );
+
+            // add_action(
+            //     "elementor/preview/enqueue_styles",
+            //     fn() => wp_enqueue_style(
+            //         "elementor-ad-eraser--elementor-preview",
+            //         Elementor_Ad_Eraser_Globals::url() .
+            //             "static/css/elementor-preview.css",
+            //         [],
+            //         Elementor_Ad_Eraser_Globals::$version
+            //     )
+            // );
+
+            // Dequeue elementor ai styles and scripts
 
             add_action(
                 "elementor/editor/after_enqueue_styles",
-                fn() => wp_enqueue_style(
-                    "elementor-ad-eraser--elementor-editor",
-                    Elementor_Ad_Eraser_Globals::url() .
-                        "static/css/elementor-editor.css",
-                    [],
-                    Elementor_Ad_Eraser_Globals::$version
-                )
+                function () {
+                    wp_dequeue_style("elementor-ai-editor");
+                },
+                100
             );
 
             add_action(
                 "elementor/preview/enqueue_styles",
-                fn() => wp_enqueue_style(
-                    "elementor-ad-eraser--elementor-preview",
-                    Elementor_Ad_Eraser_Globals::url() .
-                        "static/css/elementor-preview.css",
-                    [],
-                    Elementor_Ad_Eraser_Globals::$version
-                )
+                function () {
+                    wp_dequeue_style("elementor-ai-layout-preview");
+                },
+                100
+            );
+
+            if (is_admin()) {
+                add_action(
+                    "wp_enqueue_media",
+                    function () {
+                        wp_dequeue_script("elementor-ai-media-library");
+                    },
+                    100
+                );
+            }
+
+            add_action(
+                "elementor/editor/before_enqueue_scripts",
+                function () {
+                    wp_dequeue_script("elementor-ai");
+                    wp_dequeue_script("elementor-ai-layout");
+                },
+                100
+            );
+
+            add_action(
+                "enqueue_block_editor_assets",
+                function () {
+                    wp_dequeue_script("elementor-ai-gutenberg");
+                },
+                100
+            );
+
+            add_action(
+                "admin_enqueue_scripts",
+                function () {
+                    wp_dequeue_script("elementor-ai-admin");
+                },
+                100
             );
         }
     }

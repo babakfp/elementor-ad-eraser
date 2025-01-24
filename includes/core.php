0f828d992a99a89a518f4c1c0c7f1f5e002a0022
Babak Farkhoopak <babak.bxf@gmail.com>
2025-01-25 00:52:57 +0330

    chore; originize code

--- includes/core.php
+++ includes/core.php
@@ -8,6 +8,11 @@ require_once ELEMENTOR_AD_ERASER['PATH'] . 'includes/is-gutenberg-active.php';
 if (!class_exists('Elementor_Ad_Eraser')) {
     final class Elementor_Ad_Eraser {
         public function __construct() {
+            $this->add_custom_styles();
+            $this->remove_elementor_ai();
+        }
+
+        private function add_custom_styles() {
             add_action('admin_enqueue_scripts', function () {
                 if (is_gutenberg_active()) {
                     wp_enqueue_style('elementor-ad-eraser--gutenberg', ELEMENTOR_AD_ERASER['URL'] . 'static/css/gutenberg.css', [], ELEMENTOR_AD_ERASER['VERSION']);
@@ -20,8 +25,18 @@ if (!class_exists('Elementor_Ad_Eraser')) {
 
             add_action('elementor/preview/enqueue_styles', fn() => wp_enqueue_style('elementor-ad-eraser--elementor-preview', ELEMENTOR_AD_ERASER['URL'] . 'static/css/elementor-preview.css', [], ELEMENTOR_AD_ERASER['VERSION']));
 
-            // Dequeue elementor ai styles and scripts
+            add_action(
+                'admin_enqueue_scripts',
+                function () {
+                    if (!is_plugin_active('elementor-pro/elementor-pro.php')) {
+                        wp_enqueue_style('elementor-ad-eraser--admin-ui-elementor-pro-not-active', ELEMENTOR_AD_ERASER['URL'] . 'static/css/admin-ui-elementor-pro-not-active.css', [], ELEMENTOR_AD_ERASER['VERSION']);
+                    }
+                },
+                100
+            );
+        }
 
+        private function remove_elementor_ai() {
             add_action(
                 'elementor/editor/after_enqueue_styles',
                 function () {
@@ -72,18 +87,6 @@ if (!class_exists('Elementor_Ad_Eraser')) {
 
                     // "Optimize your images to enhance site performance by using Image Optimizer". I think this is a paid plugin (or at least it requires login). Not sure if this works.
                     wp_dequeue_script('media-hints');
-                },
-                100
-            );
-
-            //
-
-            add_action(
-                'admin_enqueue_scripts',
-                function () {
-                    if (!is_plugin_active('elementor-pro/elementor-pro.php')) {
-                        wp_enqueue_style('elementor-ad-eraser--admin-ui-elementor-pro-not-active', ELEMENTOR_AD_ERASER['URL'] . 'static/css/admin-ui-elementor-pro-not-active.css', [], ELEMENTOR_AD_ERASER['VERSION']);
-                    }
                 },
                 100
             );
